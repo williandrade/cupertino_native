ecd027d70072ca2c35d09dda3d52248ad5e1b833
Viktor Lidholt <viktorlidholt@gmail.com>
2025-09-02 21:37:26 +0200

    feat: Adds dark mode toggle.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,23 +6,55 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isDarkMode = false;
+
+  void _toggleTheme() {
+    setState(() {
+      _isDarkMode = !_isDarkMode;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return CupertinoApp(home: HomePage());
+    return CupertinoApp(
+      theme: CupertinoThemeData(
+        brightness: _isDarkMode ? Brightness.dark : Brightness.light,
+      ),
+      home: HomePage(
+        isDarkMode: _isDarkMode,
+        onToggleTheme: _toggleTheme,
+      ),
+    );
   }
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({super.key, required this.isDarkMode, required this.onToggleTheme});
+
+  final bool isDarkMode;
+  final VoidCallback onToggleTheme;
 
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
         middle: const Text('Cupertino Native'),
+        trailing: CupertinoButton(
+          padding: EdgeInsets.zero,
+          onPressed: onToggleTheme,
+          child: Icon(
+            isDarkMode ? CupertinoIcons.sun_max_fill : CupertinoIcons.moon_fill,
+            size: 22,
+          ),
+        ),
       ),
       child: SafeArea(
         child: ListView(
